1ca2ed684bec777839f8c246ce208cf796107f98
lishiwen <lishiwen@wclound.cn>
2022-05-30 18:41:26 +0800

    feat: rollup.config.js build

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
 import merge from "deepmerge";
+
+// ref: https://www.npmjs.com/package/@open-wc/building-rollup
+// ref: https://npm.io/package/@open-wc/rollup-plugin-html
 import { createBasicConfig } from "@open-wc/building-rollup";
+import html from "@open-wc/rollup-plugin-html";
+
+// ref: https://www.npmjs.com/package/@rollup/plugin-node-resolve
 import nodeResolve, { DEFAULTS } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import serve from "rollup-plugin-serve";
+import commonjs from "rollup-plugin-commonjs";
+
+const isDev = process.env.R_ENV === "development";
+const isProd = process.env.R_ENV === "production";
 
 const rollupBasicConfig = createBasicConfig();
-
 export default merge(rollupBasicConfig, {
   input: "src/index.ts",
   output: [
@@ -12,14 +22,30 @@ export default merge(rollupBasicConfig, {
       dir: "dist",
       format: "es",
       entryFileNames: "[name].js",
-      chunkFileNames: "[name]-[hash].js",
+      // chunkFileNames: "[name]-[hash].js",
     },
   ],
   plugins: [
+    commonjs(),
+    isDev && html({ files: "./src/index.html", inject: false }),
     typescript(),
     nodeResolve({
       extensions: [...DEFAULTS.extensions, ".ts"],
-      moduleDirectories: ["node_modules", "web_modules"],
+      moduleDirectories: ["node_modules"],
     }),
+    // ref: https://github.com/thgh/rollup-plugin-serve
+    isDev &&
+      serve({
+        open: true,
+        contentBase: "dist",
+        host: "127.0.0.1",
+        port: 5200,
+
+        historyApiFallback: false,
+        // https: {
+        //   key: fs.readFileSync("./.workspace/certificates/server.key"),
+        //   cert: fs.readFileSync("./.workspace/certificates/server.crt"),
+        // },
+      }),
   ],
 });
